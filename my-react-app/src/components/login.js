6f7fb1f11d9426e6019a7db77ec39c5f7cbd5e97
vaishnavimandage <108632876+vaishnavimandage@users.noreply.github.com>
2023-09-07 19:52:09 +0000

    "HOS10 Submission"

---
+++ my-react-app/src/components/login.js
@@ -0,0 +1,13 @@
+//HOS10A login.js
+import React from "react";
+
+const Login = () => {
+  return (
+    <a href="https://jubilant-space-succotash-jv4r4j6xgvp2w7-6062.app.github.dev/facebook" 
+    className="btn btn-primary">
+      Login with Facebook
+    </a>
+  );
+};
+
+export default Login;
